86356a060109adf6d3daba4d221aefcd8a711403
haitaobu <102586864+haitaobu@users.noreply.github.com>
2022-05-27 19:45:16 +0800

    Add files via upload

---
+++ websocket_serve1/src/main/java/com/websocket_serve1/WebsocketServe1Application.java
@@ -0,0 +1,13 @@
+package com.websocket_serve1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketServe1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketServe1Application.class, args);
+    }
+
+}
---
+++ websocket_serve1/src/test/java/com/websocket_serve1/WebsocketServe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket_serve1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketServe1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
