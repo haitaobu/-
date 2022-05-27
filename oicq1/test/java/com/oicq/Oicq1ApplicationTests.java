2cb5058895402f3f26f118d4c7a63bf3631b2069
haitaobu <102586864+haitaobu@users.noreply.github.com>
2022-05-27 20:01:42 +0800

    Add files via upload

---
+++ oicq1/test/java/com/oicq/Oicq1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oicq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oicq1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
