04c9130f19d4552d1bf647060254686dd44fd064
haitaobu <102586864+haitaobu@users.noreply.github.com>
2022-05-27 19:48:17 +0800

    Add files via upload

---
+++ src/main/java/com/oicq/Oicq1Application.java
@@ -0,0 +1,13 @@
+package com.oicq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oicq1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oicq1Application.class, args);
+    }
+
+}
